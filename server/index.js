1c6461ba9c723f4b4fcece82c6f92787355f8cfe
Anteo Fabris <anteo8fabris@gmail.com>
2021-06-24 03:04:27 +0200

    successfully connected database to front end

--- server/index.js
+++ server/index.js
@@ -5,12 +5,15 @@ const jobsData = require('../nyc-jobs/extjobsapi.js');
 // const models = require('../database/index.js')
 const { User, Job } = require('../database/index.js')
 
+const { addUser } = require('../database/controllers/helperFunctions.js')
+
 
 const { parse, stringify } = require('flatted');
 
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, '..')));
+app.use(express.json())
 
 app.get('/api', (req, res) => {
   res.send('Your server is online and serving!');
@@ -21,6 +24,17 @@ app.get('/api/jobs', (req, res) => {
     res.json(data.data);
   });
 });
+
+app.post('/api/user', (req, res) => {
+  console.log('made it to app.post: ', req.body)
+addUser(req.body, (err, data) => {
+  if (err) {
+    console.log('error in posting new user: ', err)
+  } else {
+    res.send(data)
+  }
+})
+})
 
 
 User.sync().then(() => {
