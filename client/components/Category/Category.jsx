cc2a0caad5a8f75cebe03c42abc50709bafc2089
Brandon Galloway <brandon.galloway2@gmail.com>
2021-06-28 12:44:23 -0400

    Adding new styles to the category list - also updated the old categories with new job keywords for a more efficient serach

--- client/components/Category/Category.jsx
+++ client/components/Category/Category.jsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import css from './Category.css';
 
 const Category = (props) => {
-  return <div>{props.data[0] + '(' + props.data[1] + ')'}</div>;
+  return (
+    <div className="each-cat">{props.data[0] + '(' + props.data[1] + ')'}</div>
+  );
 };
 
 export default Category;
--- client/components/CategoryList/CategoryList.jsx
+++ client/components/CategoryList/CategoryList.jsx
@@ -1,6 +1,7 @@
 // import React, { useState, useEffect } from 'react';
 import React from 'react';
 import css from './CategoryList.css';
+import axios from 'axios';
 
 import Category from '../Category/Category.jsx';
 
@@ -12,31 +13,75 @@ class CategoryList extends React.Component {
       categories: [],
     };
 
-    this.getUnqiueCategories = this.getUnqiueCategories.bind(this);
+    // this.getUnqiueCategories = this.getUnqiueCategories.bind(this);
+    this.getJobKeywords = this.getJobKeywords.bind(this);
   }
 
-  getUnqiueCategories() {
-    const categories = {};
-    this.props.jobs.map((cat) => {
-      if (!categories[cat.job_category]) {
-        categories[cat.job_category] = 1;
-      } else {
-        categories[cat.job_category]++;
-      }
-    });
-    this.setState({ categories: Object.entries(categories) });
+  // getUnqiueCategories() {
+  //   const categories = {};
+  //   this.props.jobs.map((cat) => {
+  //     if (!categories[cat.job_category]) {
+  //       categories[cat.job_category] = 1;
+  //     } else {
+  //       categories[cat.job_category]++;
+  //     }
+  //   });
+  //   this.setState({ categories: Object.entries(categories) });
+  // }
+
+  getJobKeywords() {
+    axios
+      .get(
+        'https://data.cityofnewyork.us/resource/kpav-sd4t.json?$query=SELECT distinct(job_category)'
+      )
+      .then((res) => {
+        var ref = {};
+        // loop over response
+        for (var i = 0; i < res.data.length; i++) {
+          if (res.data[i]['job_category_1']) {
+            // for each string at job_category
+            // split by space
+            var split = res.data[i].job_category_1.split(' ');
+            // iterate over array
+            for (var j = 0; j < split.length; j++) {
+              // currentWord = current word split on comma, index 0
+              // currentWord = c
+              var current =
+                split[j][split[j].length - 1] === ','
+                  ? split[j].substring(0, split[j].length - 1)
+                  : split[j];
+              // if ref does not contain currentWord
+              if (current === '&') continue;
+              if (!ref[current]) {
+                // ref at current word equals 1
+                ref[current] = 1;
+              } else {
+                // else, ref[currentWord] ++
+                ref[current]++;
+              }
+            }
+          }
+        }
+        this.setState({
+          categories: Object.entries(ref),
+        });
+      })
+      .catch((err) => {
+        console.log('err!', err);
+      });
   }
 
   componentDidUpdate(prevProps) {
     if (prevProps.jobs !== this.props.jobs) {
-      this.getUnqiueCategories();
+      // this.getUnqiueCategories();
+      this.getJobKeywords();
     }
   }
 
   render() {
     return (
       <div className="category-container">
-        <div>Job Categories</div>
+        <div>Job Keywords</div>
         <div className="cat-list">
           {this.state.categories &&
             this.state.categories.map((category, idx) => {
